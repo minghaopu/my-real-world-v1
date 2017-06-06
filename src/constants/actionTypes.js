6dff743118063b050ba2dbbf69d4be325a7d352c
roneil <roneil_PMH@hotmail.com>
2017-06-05 17:47:43 -0700

    Configure Project; create directory;

---
+++ src/constants/actionTypes.js
@@ -0,0 +1,23 @@
+// User actions
+
+export const USER_REQUEST = 'USER_REQUEST';
+export const USER_SUCCESS = 'USER_SUCCESS';
+export const USER_FAULURE = 'USER_FAULURE';
+
+// Repo actions
+
+export const REPO_REQUEST = 'REPO_REQUEST';
+export const REPO_SUCCESS = 'REPO_SUCCESS';
+export const REPO_FAILURE = 'REPO_FAILURE';
+
+// Stared Repo actions
+
+export const STARRED_REQUEST = 'STARRED_REQUEST';
+export const STARRED_SUCCESS = 'STARRED_SUCCESS';
+export const STARRED_FAILURE = 'STARRED_FAILURE';
+
+// Stargazers actions
+
+export const STARGAZERS_REQUEST = 'STARGAZERS_REQUEST';
+export const STARGAZERS_SUCCESS = 'STARGAZERS_SUCCESS';
+export const STARGAZERS_FAILURE = 'STARGAZERS_FAILURE';
